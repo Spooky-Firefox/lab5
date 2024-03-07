83cc2a1a949a57084d9d6d0240e3a9546db819bd
Olle Ronstad <olle.ronstad@gmail.com>
2024-03-07 11:38:31 +0100

    tmp code in main

--- lab5/main.c
+++ lab5/main.c
@@ -9,12 +9,17 @@
 
 #include "TinyTimber.h"
 #include "Serial_Writer.h"
+#include "Controller.h"
+#include "Serial_Reader.h"
 
 struct Serial_Writer ser_wri = initSerial_writer();
+struct Controller cntr = initController(&ser_wri);
+struct Serial_Reader ser_rea = innitSerial_Reader(&cntr);
 
 int main(void)
 {
     init_uart();
-    TINYTIMBER(&ser_wri, write_cyclic, NULL);
+    UDR0 = 0xaa;
+    TINYTIMBER(&ser_rea, init_interrupt, NULL);
 }
 
